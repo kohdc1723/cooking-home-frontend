e8cb47541946023cc8e7fb8d8205e7e8d448b463
kohdc <kohdc1723@gmail.com>
2023-12-19 17:04:48 -0800

    improving ui, mobile friendly

--- src/features/recipe/components/RecipeResult.jsx
+++ src/features/recipe/components/RecipeResult.jsx
@@ -55,7 +55,7 @@ const RecipeResult = () => {
         const to = Math.min(page * 10, count);
 
         return (
-            <div className="h-screen pt-[136px] lg:px-20 flex justify-center bg-slate-100">
+            <div className="h-screen pt-[136px] lg:px-20 flex flex-1 justify-center bg-slate-100">
                 {ids?.length ? (
                     <div className="flex shadow-lg flex-1">
                         <div className="flex flex-col flex-1">
